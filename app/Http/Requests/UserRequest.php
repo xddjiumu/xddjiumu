90511278299d2ecc3472a4ce52ec82942e2c30a8
liuwei <liuweihjyalms@163.com>
2019-03-02 03:26:39 +0000

    userRequest 字段正则 用户信息编辑支持中字名

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,7 +25,7 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'name' => 'required|between:2,25|regex:/^[\x{4e00}-\x{9fa5}A-Za-z0-9-_]+$/u|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
             'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
