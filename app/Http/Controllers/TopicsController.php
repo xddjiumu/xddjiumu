204ea7da0109e42e0be4ec77be9de1a08460728e
liuwei <liuweihjyalms@163.com>
2019-03-05 13:05:46 +0000

    use Model model

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Topic;
-use App\models\Category;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
