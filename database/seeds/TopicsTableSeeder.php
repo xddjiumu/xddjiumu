bb24ce20a7567ae8b81b18e8f9c7689ee86efee8
liuwei <liuweihjyalms@163.com>
2019-03-15 03:40:23 +0000

    话题数据

---
+++ database/seeds/TopicsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Topic;
+use App\Models\Category;
+
+class TopicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 所有分类 ID 数组，如：[1,2,3,4]
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+                        ->times(100)
+                        ->make()
+                        ->each(function ($topic, $index)
+                            use ($category_ids, $faker)
+        {
+            // 从话题分类 ID 数组中随机取出一个并赋值
+            $topic->category_id = $faker->randomElement($category_ids);
+        });
+
+        // 将数据集合转换为数组，并插入到数据库中
+        Topic::insert($topics->toArray());
+    }
+}
