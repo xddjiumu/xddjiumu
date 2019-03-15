0d7d8387d3809bf88918697f42bd42a4deefd786
liuwei <liuweihjyalms@163.com>
2019-03-15 07:10:28 +0000

    分类话题列表 导航的 Active 状态

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -8,7 +8,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
+        <li class="nav-item {{ active_class(if_route('root')) }}">
           <a class="nav-link" href="{{ url('/') }}">站点首页 <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
@@ -17,13 +17,13 @@
         <li class="nav-item">
           <a class="nav-link" href="#">产品中心</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ category_nav_active(1) }}">
           <a class="nav-link" href="{{ route('categories.show', 1) }}">新闻资讯</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ category_nav_active(2) }}">
           <a class="nav-link" href="{{ route('categories.show', 2) }}">公司动态</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ active_class((if_route('contact.index'))) }}">
           <a class="nav-link" href="{{ route('contact.index') }}">联系我们</a>
         </li>
       </ul>
