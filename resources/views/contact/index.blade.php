2a0cff6dc08895dc3e8a820084543ecd2f91748d
liuwei <liuweihjyalms@163.com>
2019-03-21 01:10:00 +0000

    map too

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -101,21 +101,7 @@
   </div>
 </section>
 
-<section class="pt-4">
-  <script type="text/javascript" src="http://api.map.baidu.com/api?v=3.0&ak=9oY4Y1e8aYrTh9EhbDmZUFLGKuKgxE4M&s=1"></script>
-  <div id="allmap" class="w-100" style="height: 500px;"></div>
-  <script type="text/javascript">
-    // 百度地图API功能
-    var map = new BMap.Map("allmap");// 创建Map实例
-    map.centerAndZoom(new BMap.Point(113.035682,28.173575), 12);// 初始化地图,设置中心点坐标和地图级别
-    //添加地图类型控件
-    map.addControl(new BMap.MapTypeControl({
-      mapTypes:[
-              BMAP_NORMAL_MAP,
-              BMAP_HYBRID_MAP
-          ]}));   
-    map.setCurrentCity("长沙");// 设置地图显示的城市 此项是必须设置的
-    map.enableScrollWheelZoom(true);//开启鼠标滚轮缩放
-  </script>
+<section>
+  <iframe src="/map.html" class="w-100" style="height: 35rem;border: 0 solid #fff"></iframe>
 </section>
 @stop
