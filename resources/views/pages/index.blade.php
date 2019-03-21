778369ae6a33e15cbc8dde0f3e6ecb5b43b999f3
liuwei <liuweihjyalms@163.com>
2019-03-21 03:30:49 +0000

    brand text

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
   <p class="pb-5 px-2"></p>
   <div class="row p-2 m-0">
     <div class="col-12 col-sm-8 m-auto">
-      <img alt="Bell - A perfect theme" class="w-100" src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/gadgets.png">
+      <img alt="Bell - A perfect theme" class="w-100" src="/images/indexgadgets.png">
     </div>
   </div>
 </div>
@@ -14,14 +14,14 @@
   <div class="container pt-5">
     <div class="row">
       <div class="col-md-6">
-        <p><strong class="">湖南响当当九牧管业有限公司</strong>是—家专业从事塑料管道产品研究和制造的高新技术企业，公司采用具有国际先进水平的现代化生产设备、专业检测设备和进口优质原料，以传统的制管工艺为依托，传承现代化科技精髓，产品达到国际健康管道标准。企业严格执行IS09001:2008国际质量管理体系认证，产品由由中国人民保险公司承保，荣获"工程建设推荐产品"、 "苏州名牌产品"等荣誉称号，并获得全国二十多个省市自治区的推广证书，深得市场及消费者的认可与青睐。</p>
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-        <p>湖南响当当九牧管业有限公司是—家专业从事塑料管道产品研究和制造的高新技术企业，公司采用具有国际先进水平的现代化生产设备、专业检测设备和进口优质原料，以传统的制管工艺为依托，传承现代化科技精髓，产品达到国际健康管道标准。企业严格执行IS09001:2008国际质量管理体系认证，产品由由中国人民保险公司承保，荣获"工程建设推荐产品"、 "苏州名牌产品"等荣誉称号，并获得全国二十多个省市自治区的推广证书，深得市场及消费者的认可与青睐。</p>
+        <p class="text-indent"><strong class="text-primary" style="font-size: 1.3rem">湖南响当当九牧管业有限公司</strong>是—家专业从事塑料管道产品研究和制造的高新技术企业，公司采用具有国际先进水平的现代化生产设备、专业检测设备和进口优质原料，以传统的制管工艺为依托，传承现代化科技精髓，产品达到国际健康管道标准。企业严格执行IS09001:2008国际质量管理体系认证，产品由由中国人民保险公司承保，荣获"工程建设推荐产品"、 "苏州名牌产品"等荣誉称号，并获得全国二十多个省市自治区的推广证书，深得市场及消费者的认可与青睐。</p>
+        <img src="/images/about_pic.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <p class="text-indent">公司以改善全民用水品质，解决水质健康隐患为目标，为中国千万个家庭带来全球领先的健康管道解决方案。公司依托强大的资金实力与雄厚、丰富的资源供应，购置超一流的全自动注塑设备、—体化生产流水线，同步引进世界领先的产品检测设备，专业生产PP-R管材管件、FR/PP-R玻纤增强管材管件、纳米抗菌PP-R管材管件、PE给水管、PVC-U排水管、PVC-U阻燃电工管等管道系列产品。同时，企业在管道行业创新与发展的背景与趋势下，积极探索中国管道产业健康发展与改革之路。通过多年的自主创新与积累，基于产品优质的原材料、先进的生产工艺及优良品质、以及多年来长期安全可靠的市场表现，公司将传统饮用水管道再次创新，借鉴世界领先的产品理念，打造专业管道精品，成就辉煌卓越品质。</p>
       </div>
       <div class="col-md-6">
-        <p><strong class="">湖南响当当九牧管业有限公司</strong>是—家专业从事塑料管道产品研究和制造的高新技术企业，公司采用具有国际先进水平的现代化生产设备、专业检测设备和进口优质原料，以传统的制管工艺为依托，传承现代化科技精髓，产品达到国际健康管道标准。企业严格执行IS09001:2008国际质量管理体系认证，产品由由中国人民保险公司承保，荣获"工程建设推荐产品"、 "苏州名牌产品"等荣誉称号，并获得全国二十多个省市自治区的推广证书，深得市场及消费者的认可与青睐。</p>
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-        <p>湖南响当当九牧管业有限公司是—家专业从事塑料管道产品研究和制造的高新技术企业，公司采用具有国际先进水平的现代化生产设备、专业检测设备和进口优质原料，以传统的制管工艺为依托，传承现代化科技精髓，产品达到国际健康管道标准。企业严格执行IS09001:2008国际质量管理体系认证，产品由由中国人民保险公司承保，荣获"工程建设推荐产品"、 "苏州名牌产品"等荣誉称号，并获得全国二十多个省市自治区的推广证书，深得市场及消费者的认可与青睐。</p>
+        <img src="/images/about_pic1.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <p class="text-indent">作为—家集科研、环保管道开发、生产、销售为—体的优秀现代化企业，公司以其优异的质量，良好的形象，多年品牌化市场运作的积淀，已成功建立起成熟的市场基础与品牌信誉。凭借成熟的市场运作经验、全面的渠道分析把控能力，公司产品已消往全国各地，并设立了办事处和特约经销商3000多家，成功构建起诸多品牌强势区域。公司完善的服务也赢得了广大代理商以及装饰、建筑、工程等知名品牌战略合作伙伴的信赖与支持。</p>
+        <p class="text-indent">展望未来，公司将坚持走品牌之路，将国际先进的技术工艺与家居洁水理念带入现代家庭，以最高端的产品质量，最贴心的售后服务，关注每—个中国家庭的饮用水安全。</p>
       </div>
     </div>
   </div>
@@ -39,54 +39,39 @@
   </div>
 </section>
 
-<div class="text-center indexblock text-white pt-5 my-2" style="background-image: url('http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg'); background-position: 0px -52.4px;">
+<div class="text-center text-white p-5 my-2" style="background-image: url('http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg'); background-position: 0px -52.4px;">
   <h2 class="px-2 pt-5">品牌形像</h2>
-  <p class="pb-5 px-2"></p>
-  <div class="row p-2 m-0">
-    <div class="col-12 col-sm-8 m-auto">
-      <img alt="Bell - A perfect theme" class="w-100" src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/gadgets.png">
-    </div>
-  </div>
 </div>
 
 <section>
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/1.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/2.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/3.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/4.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/5.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/6.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/7.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/8.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/9.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
     </div>
   </div>
@@ -107,14 +92,8 @@
   </div>
 </section>
 
-<div class="text-center indexblock text-white pt-5 my-2" style="background-image: url('http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg'); background-position: 0px -52.4px;">
+<div class="text-center text-white p-5 my-2" style="background-image: url('http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg'); background-position: 0px -52.4px;">
   <h2 class="px-2 pt-5">品牌理念</h2>
-  <p class="pb-5 px-2"></p>
-  <div class="row p-2 m-0">
-    <div class="col-12 col-sm-8 m-auto">
-      <img alt="Bell - A perfect theme" class="w-100" src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/gadgets.png">
-    </div>
-  </div>
 </div>
 
 <section>
@@ -140,7 +119,7 @@
         <p class="text-secondary"><strong class="text-primary">营销理念：</strong>要卖好产品 先做好人品</p>
       </div>
       <div class="col-md-4">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/about_pic2.jpg" class="img-fluid mb-3" alt="Responsive image">
       </div>
     </div>
   </div>
@@ -158,54 +137,24 @@
   </div>
 </section>
 
-<div class="text-center indexblock text-white pt-5 my-2" style="background-image: url('http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg'); background-position: 0px -52.4px;">
+<div class="text-center text-white p-5 my-2" style="background-image: url('http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg'); background-position: 0px -52.4px;">
   <h2 class="px-2 pt-5">资质荣誉</h2>
-  <p class="pb-5 px-2"></p>
-  <div class="row p-2 m-0">
-    <div class="col-12 col-sm-8 m-auto">
-      <img alt="Bell - A perfect theme" class="w-100" src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/gadgets.png">
-    </div>
-  </div>
 </div>
 
 <section>
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/21.gif" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/22.gif" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/23.gif" class="img-fluid mb-3" alt="Responsive image">
       </div>
       <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
-      </div>
-      <div class="col-md-3 col-sm-6">
-        <img src="http://demo.cssmoban.com/cssthemes5/twts_169_bell/img/parallax-bg.jpg" class="img-fluid mb-3" alt="Responsive image">
+        <img src="/images/24.gif" class="img-fluid mb-3" alt="Responsive image">
       </div>
     </div>
   </div>
